0ee7b8c5f10d32280cd86485089e4c574c1a3fc7
jacob.shuman <jacob.shuman7@gmail.com>
2018-10-26 18:35:32 -0400

    updated build/index

--- build/index.js
+++ build/index.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const search_1 = require("./search");
-const lookup_1 = require("./lookup");
-exports.default = {
-    search: search_1.search,
-    lookup: lookup_1.lookup
-};
+var search_1 = require("./search");
+exports.search = search_1.search;
+var lookup_1 = require("./lookup");
+exports.lookup = lookup_1.lookup;
