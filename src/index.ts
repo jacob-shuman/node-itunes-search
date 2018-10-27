028cac9faea8e553ab257d2542956692b6a56d30
jacob.shuman <jacob.shuman7@gmail.com>
2018-10-27 18:32:46 -0400

    exporting ItunesMedia from module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { searchItunes, ItunesSearchOptions } from "./search";
 export { lookupItunes, ItunesLookupOptions } from "./lookup";
 export { ItunesResult } from "./result";
+export { ItunesMedia } from "./media";
